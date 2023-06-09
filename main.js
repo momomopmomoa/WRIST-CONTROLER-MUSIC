1974f48bc5c8fe9b0c8036389094027b987d714e
abhinav jha <116362054+momomopmomoa@users.noreply.github.com>
2023-06-09 07:27:45 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 var song1 = "music.mp3";
 var song2 = "Harry-Potter-Theme.mp3";
+RightWristX = 0;
+RightWristY = 0;
+LeftWristX = 0;
+LeftWristY = 0;
 
 function preload(){
     song1 = loadSound("music.mp3");
@@ -15,6 +19,21 @@ function setup(){
 
     poseNet = ml5.poseNet(video, modelLoaded);
     poseNet.on('pose', gotPoses);
+}
+
+function modalLoaded(){
+    console.log('POSENET IS INITIALIZED')
+}
+
+function gotPoses(results){
+    console.log(results.length);
+    if(results.length > 0){
+        console.log(results);
+        RightWristX = results[0].pose.rightwrist.x;
+        RightWristY = results[0].pose.rightwrist.y;
+        LeftWristX = results[0].pose.leftwrist.x;
+        LeftWristY = results[0].pose.leftwrist.y;
+    }
 }
 
 function draw(){
